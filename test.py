da05cb3b162587202bcbd0c0b3e142064fed16fd
khanh <nguyenngockhanh.pbc@gmail.com>
2021-08-26 21:10:58 +0800

    updated test

--- test.py
+++ test.py
@@ -8,16 +8,15 @@ from suls import solve_lp, solve_l1
 np.random.seed(1234)
 
 
-def draw_hist(x: np.ndarray, title: str = "norm", draw: bool = False):
+def draw_hist(ax: plt.Axes, x: np.ndarray, title: str = ""):
     hist, edge = np.histogram(x, bins=101, range=[-0.1, +0.1])
     center = np.array([0.5 * (edge[i] + edge[i + 1]) for i in range(len(hist))])
     print(f"middle [{edge[int(len(hist) / 2)]}, {edge[1 + int(len(hist) / 2)]}] occurences: {hist[int(len(hist) / 2)]}")
-    if draw:
-        plt.title(title)
-        plt.xlabel("values")
-        plt.ylabel("occurrences")
-        plt.bar(center, hist, width=(center[1] - center[0]))
-        plt.show()
+    ax.bar(center, hist, width=(center[1] - center[0]))
+    ax.set_title(title)
+    ax.set_xlabel("values")
+    ax.set_ylabel("occurrences")
+    ax.set_title(title)
 
 
 def norm_p(x: np.ndarray, p: float = 2.0) -> float:
@@ -29,7 +28,10 @@ m = 400
 A = np.random.random(size=(m, n)).astype(dtype=np.float64)
 b = np.random.random(size=(m)).astype(dtype=np.float64)
 
-for p in [1, 1.000001, 1.5, 2.0]:
+fig, ax = plt.subplots(nrows=2, ncols=2)
+ax = ax.flatten()
+
+for i, p in enumerate([1, 1.000001, 1.5, 2.0]):
     t0 = time.time()
     if p == 1:
         x = solve_l1(A, b)
@@ -39,6 +41,7 @@ for p in [1, 1.000001, 1.5, 2.0]:
     print(f"L^{p} time: {t1 - t0}")
     print(f"\tconstraints: {np.max(np.abs(A @ x - b))}")
     print(f"\tL^p norm: {norm_p(x, p)}")
-    draw_hist(x, f"L^{p} norm", draw=True)
+    draw_hist(ax[i], x, f"L^{p} norm")
 
-pass
+plt.tight_layout()
+plt.show()
